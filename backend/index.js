38956cfbe49ee4e4b64bfa7956e5214f85fc4fbb
AnsaAli <chinjuansa@example.com>
2024-08-15 17:38:48 +0530

    connect to DB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to the mongo"))
+  .catch((err) => console.log(err));
+
+
 const app = express();
 app.use(express.json());
 app.listen(3000, () => console.log("Server running on 3000"));
