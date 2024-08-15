4ee10ee4650043dfca2047e91f91363742d3c2bc
AnsaAli <chinjuansa@example.com>
2024-08-15 20:15:42 +0530

    add basic validation for signup form

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,3 +1,4 @@
+import { errorHandler } from "../../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/userModel.js";
 
@@ -9,7 +10,7 @@ export const signup = async (req, res, next) => {
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
-
+    
     // Create a hashed password
     const hashPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashPassword });
@@ -20,3 +21,16 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin =async (req,res,next)=>{
+  const {email, password} = req.body;
+  try {
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404, 'User no found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword) return next(errorHandler(401, 'Invalid email or password'));
+    // const token = jwt.sign({id: validUser._id}, process.env.JWT_Sectret);
+  } catch (error) {
+    next(error);
+  }
+}
