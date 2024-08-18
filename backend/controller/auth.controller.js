f7885df1e819ba7fe41edcc4c40576ba5944dd86
AnsaAli <chinjuansa@example.com>
2024-08-18 10:49:45 +0530

    create and add OAuth component

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -31,7 +31,7 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword)
       return next(errorHandler(401, "Invalid email or password"));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_Sectret);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_Secret);
     //no need to send the password, for safty purpose
     const { password: hashPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000);
@@ -40,6 +40,56 @@ export const signin = async (req, res, next) => {
       .status(200)
       .json(rest);
   } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  console.log("inside google controller");
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      console.log("google contrller in if user");
+      const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_Secret || "default-secret-key"
+      );
+      const { password: hashPassword, ...rest } = user._doc;
+      const expiryDate = new Date(Date.now() + 3600000);
+      res
+        .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json(rest);
+    } else {
+      console.log("google contrller in else user");
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.floor(Math.random() * 10000).toString(),
+        email: req.body.email,
+        password: hashedPassword,
+        profilePicture: req.body.photo,
+      });
+
+      await newUser.save();
+
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_Secret);
+      const { password: hashPassword, ...rest } = newUser._doc;
+      const expiryDate = new Date(Date.now() + 3600000);
+
+      res
+        .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json(rest);
+    }
+  } catch (error) {
+    console.log("error in google controller", error);
     next(error);
   }
 };
--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { signin, signup } from '../controller/auth.controller.js';
+import { google, signin, signup } from '../controller/auth.controller.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup',signup);
 authRouter.post('/signin', signin);
+// authRouter.post('/google',google)
+authRouter.post('/google', (req, res, next) => {
+    console.log('Received request on /google route:', req.body);
+    next();
+  }, google);
+  
 
 
 export default authRouter;
